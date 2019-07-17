a83d1673bd029d0451bcc6572b437df2b4f20faa
Andreé Toledo <edgar.andree20@gmail.com>
2019-07-16 19:53:49 -0600

    aded isbn verificador

--- Algebra_Lineal_1_Challenge_1_Edgar_Toledo_Javier_Salazar.py
+++ Algebra_Lineal_1_Challenge_1_Edgar_Toledo_Javier_Salazar.py
@@ -7,6 +7,22 @@
 
 import os
 
+"Verifica si es ISBN"
+def verify(isbn):
+    isbn = input("Ingrese el isbn")
+    counter = 1
+    total = 0
+    for d in reversed(isbn):
+        if d is 'X':
+            if counter != 1:
+                return False
+            d = '10'
+        if d.isdigit():
+            total += int(d) * counter
+            counter += 1
+    return counter == 11 and total % 11 == 0
+
+
 
 """
 Esto elimina el número de cualquier separador válido y
