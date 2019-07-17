cb089e2aa26727ddc2a1f552f3a4909333aa86d0
Andreé Toledo <edgar.andree20@gmail.com>
2019-07-17 00:42:50 -0600

    Update isbn_verifier.py

--- isbn_verifier.py
+++ isbn_verifier.py
@@ -14,3 +14,29 @@ def verify(isbn):
             total += int(d) * counter
             counter += 1
     return counter == 11 and total % 11 == 0
+
+
+
+
+from itertools import cycle
+
+ISBN_13 = input('Ingrese el codigo a verificar')
+
+def digito_verificador(ISBN_13):
+    reversed_digits = map(int, reversed(str(ISBN_13)))
+    factors = [1,3,1,3,1,3,1,3,1,3,1,3,1]
+    s = sum(d * f for d, f in zip(reversed_digits, factors))
+    return (-s) % 10
+
+print(digito_verificador(ISBN_13))
+
+#v = [9,7,0,6,8,6,2,7,2,2]
+
+#c = [10,9,8,7,6,5,4,3,2,1]
+
+#vc = [a*b for a,b in zip(v,c)]
+
+#for i in vc:
+ #   i = int (i)
+  #  mod = i%11
+   # print (mod)
